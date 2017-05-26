1ceef71a7b12bd726f6802dd0ded699eea8708ba
boujnane monssef <m-heroine@hotmail.com>
2017-05-26 20:24:05 +0000

    left side bar component

---
+++ src/components/LeftSideBar.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from "react";
+import Styled from "styled-components";
+
+import LeftSideBarSection from "./LeftSideBarSection";
+
+const SideBarWrapper = Styled.div`
+  flex: 1;
+  min-height: 100vh;
+  height: 100%;
+  background-color: #363949;
+  border-right: 1px solid #2f3241;
+`;
+
+class LeftSideBar extends PureComponent {
+  render() {
+    return (
+      <SideBarWrapper>
+        <LeftSideBarSection
+          title="Main"
+          items={[
+            { title: "Movies" },
+            { title: "tv Shows" },
+            { title: "Tv Schedule" }
+          ]}
+          selected="movies"
+        />
+      </SideBarWrapper>
+    );
+  }
+}
+
+export default LeftSideBar;
