3b9a94b953747bb1220599bbed515c5f7c8eda9d
boujnane monssef <m-heroine@hotmail.com>
2017-05-26 20:23:27 +0000

    solution for dynamic import icons

---
+++ src/assets/icons.js
@@ -0,0 +1,13 @@
+import movies from "react-icons/lib/md/local-movies";
+import shows from "react-icons/lib/md/live-tv";
+import schedule from "react-icons/lib/md/schedule";
+import search from "react-icons/lib/md/search";
+
+const icons = {
+  movies: movies,
+  tv_shows: shows,
+  tv_schedule: schedule,
+  search
+};
+
+export default icons;
