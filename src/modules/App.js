51e12cccb66e7bda9ea3fd42ff1cdfcef2061ddb
boujnane monssef <m-heroine@hotmail.com>
2017-05-27 14:15:03 +0000

    added route for show page

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Home from "./home/HomeContainer";
+
+import store from "../store/store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={Home} />
-          <Route path="/hi" component={Home} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={Home} />
+            <Route path="/hi" component={Home} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
