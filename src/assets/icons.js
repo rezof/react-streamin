ac848d70f4273072c3e6e2c0ab3e169c765a8039
boujnane monssef <m-heroine@hotmail.com>
2017-05-27 14:05:20 +0000

    added more, plus, play icons

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -2,12 +2,18 @@ import movies from "react-icons/lib/md/local-movies";
 import shows from "react-icons/lib/md/live-tv";
 import schedule from "react-icons/lib/md/schedule";
 import search from "react-icons/lib/md/search";
+import plus from "react-icons/lib/md/add";
+import more from "react-icons/lib/md/keyboard-control";
+import play from "react-icons/lib/md/play-arrow";
 
 const icons = {
   movies: movies,
   tv_shows: shows,
   tv_schedule: schedule,
-  search
+  search,
+  plus,
+  more,
+  play
 };
 
 export default icons;
