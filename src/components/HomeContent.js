3127cad4cc4240a0abfe870eb95736f17e01b66a
boujnane monssef <m-heroine@hotmail.com>
2017-05-27 14:13:34 +0000

    displaying data

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -6,7 +6,7 @@ import HomeContentItem from "./HomeContentItem";
 import LeftSideBar from "./LeftSideBar";
 
 const Container = Styled.div`
-    display: flex;
+  display: flex;
 `;
 
 const ContentWrapper = Styled.div`
@@ -21,16 +21,29 @@ const ItemsList = Styled.div`
     min-width: 80%;
 `;
 
+const LoadingData = Styled.p`
+  width: 100%;
+  text-align: center;
+  color: white;
+`;
+
 const data = Array.apply(null, { length: 10 }).map((key, index) => index);
 
 export default props => {
+  const { data, loading_data } = props;
+  let content = null;
+  if (loading_data) {
+    content = <LoadingData>loading...</LoadingData>;
+  } else {
+    content = data.map(item => <HomeContentItem key={item.id} item={item} />);
+  }
   return (
     <Container>
       <LeftSideBar />
       <ContentWrapper>
         <HomeHeader />
         <ItemsList>
-          {data.map(item => <HomeContentItem key={item} />)}
+          {content}
         </ItemsList>
       </ContentWrapper>
     </Container>
