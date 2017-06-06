c6344fd0e00d73c1f4f59ab8e360115c0223a33b
boujnane monssef <m-heroine@hotmail.com>
2017-06-06 22:49:58 +0000

    added movies reducer to the store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux-loop-symbol-ponyfill";
 
 import homeState from "../modules/home/HomeState";
 import showState from "../modules/show/ShowState";
+import moviesState from "../modules/movies/MoviesState";
 
 const reducers = combineReducers({
   homeState,
-  showState
+  showState,
+  moviesState
 });
 
 export default reducers;
