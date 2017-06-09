1103c3d2da5b9afe5ce3c002cfda9c2b42155b40
boujnane monssef <m-heroine@hotmail.com>
2017-06-09 23:32:49 +0000

    added movie page state to the store

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux-loop-symbol-ponyfill";
 import homeState from "../modules/home/HomeState";
 import showState from "../modules/show/ShowState";
 import moviesState from "../modules/movies/MoviesState";
+import movieState from "../modules/movie/MovieState";
 
 const reducers = combineReducers({
   homeState,
   showState,
-  moviesState
+  moviesState,
+  movieState
 });
 
 export default reducers;
