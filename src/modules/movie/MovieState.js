f6f35a520ba0217341dfe4ea60647b212dbecd73
boujnane monssef <m-heroine@hotmail.com>
2017-06-15 23:50:46 +0000

    added credits fetching action

--- src/modules/movie/MovieState.js
+++ src/modules/movie/MovieState.js
@@ -3,6 +3,7 @@ import { loop, Effects } from "redux-loop-symbol-ponyfill";
 
 const LOAD_MOVIE_DETAILS = "LOAD_MOVIE_DETAILS";
 const MOVIE_DETAILS_LOADED = "MOVIE_DETAILS_LOADED";
+const MOVIE_CAST_LOADED = "MOVIE_CAST_LOADED";
 
 type actionType = {
   type: string,
@@ -18,8 +19,41 @@ const load_movie_details = (id: number) => {
   )
     .then(r => r.json())
     .then(movie_details_loaded)
+    .catch(err => {
+      console.log("thrown", err);
+    });
+};
+
+const filter_cast_from_credits = data => {
+  if (
+    data &&
+    data.hasOwnProperty("cast") &&
+    typeof data.cast === "object" &&
+    "slice" in data.cast
+  ) {
+    return data.cast.slice(0, 20);
+  } else {
+    return [];
+  }
+};
+
+const load_movie_credits = (id: number) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/credits?api_key=571f89e8e2fdb653f0b2bb281fa8f241`
+  )
+    .then(res => res.json())
+    .then(filter_cast_from_credits)
+    .then(data => {
+      return data;
+    })
+    .then(movie_cast_loaded_action)
     .catch();
 };
+
+export const movie_cast_loaded_action = (payload): actionType => ({
+  type: MOVIE_CAST_LOADED,
+  payload
+});
 
 export const load_movie_details_action = (id: number): actionType => ({
   type: LOAD_MOVIE_DETAILS,
@@ -162,9 +196,21 @@ const MovieStateReducer = (
       );
     case MOVIE_DETAILS_LOADED:
       const { data: movie } = payload;
+      return loop(
+        {
+          ...state,
+          loading_movie_details: false,
+          movie
+        },
+        Effects.promise(load_movie_credits.bind(null, movie.id))
+      );
+    case MOVIE_CAST_LOADED:
+      const { movie: _movie } = state;
+      const new_movie = Object.assign({}, _movie, { cast: payload });
       return {
-        loading_movie_details: false,
-        movie
+        ...state,
+        loading_movie_cast: false,
+        movie: new_movie
       };
     default:
       return state;
