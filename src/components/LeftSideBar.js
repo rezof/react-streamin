a61961c2e595d21986bdf1bfb8fb64a555dd371b
boujnane monssef <m-heroine@hotmail.com>
2017-06-18 23:10:17 +0000

    switched to stateless function

--- src/components/LeftSideBar.js
+++ src/components/LeftSideBar.js
@@ -5,46 +5,43 @@ import Styled from "styled-components";
 import CurrentPositionSection from "./CurrentPositionSection";
 
 const SideBarWrapper = Styled.div`
-  flex: 1;  
+  flex: 1;
 `;
 
 type propsType = {
+  name: string,
   location: {
     pathname: string
   }
 };
 
 const defaultProps = {
+  name: "",
   location: {
     pathname: ""
   }
 };
 
-class LeftSideBar extends PureComponent {
-  props: propsType;
-  static defaultProps = defaultProps;
-
-  render() {
-    const { location, name } = this.props;
-    return (
-      <SideBarWrapper>
-        <CurrentPositionSection
-          name={name}
-          location={location.pathname}
-          title="Main"
-          items={[
-            { title: "tv Shows", to: "/", sub: { pattern: "/show/[0-9]*$" } },
-            {
-              title: "Movies",
-              to: "/movies",
-              sub: { pattern: "/movie/[0-9]*$" }
-            },
-            { title: "Tv Schedule", to: "/schedule" }
-          ]}
-        />
-      </SideBarWrapper>
-    );
-  }
-}
+const LeftSideBar = (props: propsType = defaultProps) => {
+  const { location, name } = props;
+  return (
+    <SideBarWrapper>
+      <CurrentPositionSection
+        name={name}
+        location={location.pathname}
+        title="Main"
+        items={[
+          { title: "tv Shows", to: "/", sub: { pattern: "/show/[0-9]*$" } },
+          {
+            title: "Movies",
+            to: "/movies",
+            sub: { pattern: "/movie/[0-9]*$" }
+          },
+          { title: "Tv Schedule", to: "/schedule" }
+        ]}
+      />
+    </SideBarWrapper>
+  );
+};
 
 export default LeftSideBar;
