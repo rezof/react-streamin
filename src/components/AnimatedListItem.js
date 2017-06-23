491956ced1d0f88e07c0f86a8cfa1e2c57c36ecb
boujnane monssef <m-heroine@hotmail.com>
2017-06-23 22:55:52 +0000

    deleted dead code

--- src/components/AnimatedListItem.js
+++ src/components/AnimatedListItem.js
@@ -8,35 +8,14 @@ const wrapper = Styled.div`
 `;
 
 class AnimatedListItem extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      animate: false
-    };
-  }
-
-  componentWillMount() {
-    const { mountAnimation } = this.props;
-    this.setState({ animationName: mountAnimation });
-  }
-
   render() {
-    const { children, duration, animate, unMountAnimation } = this.props;
+    const { children, duration, animation } = this.props;
     let Wrapper = wrapper;
-    if (animate) {
-      const Animation = animations[unMountAnimation];
-      const KeyFrameAnimation = keyframes`${Animation}`;
-      Wrapper = Wrapper.extend`
-            animation: ${duration}s ${KeyFrameAnimation};
-      `;
-    } else {
-      const { animationName } = this.state;
-      const Animation = animations[animationName];
-      const KeyFrameAnimation = keyframes`${Animation}`;
-      Wrapper = Wrapper.extend`
-            animation: ${duration}s ${KeyFrameAnimation};
-      `;
-    }
+    const Animation = animations[animation];
+    const KeyFrameAnimation = keyframes`${Animation}`;
+    Wrapper = Wrapper.extend`
+          animation: ${duration}s ${KeyFrameAnimation};
+    `;
     return (
       <Wrapper>
         {children}
